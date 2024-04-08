bbbfa6dcaf584bbc471d0812213e62d28ba85efc
Juan Carlos Llanos <97850510+Juanllanos19@users.noreply.github.com>
2024-04-07 23:13:30 -0600

    Comenzamos con la primer prueba de la clase TestsFloats

--- clite.py
+++ clite.py
@@ -1,15 +1,25 @@
 import ply.lex as lex
 
-tokens = [ 'INT' ]
+tokens = ['FLOAT', 'INT']  
 
-t_ignore  = ' \t'
+t_ignore = ' \t'
+
+
+def t_FLOAT(t):
+    r'\d+(\.\d*)?(e[-+]?\d+)?|\.\d+(e[-+]?\d+)?'  
+    t.value = float(t.value)
+    return t
+
+
 
 def t_INT(t):
-    r'\d+(?:_\d+)*' 
+    r'\d+(?:_\d+)*'  
     t.value = int(t.value.replace('_', ''))
     return t
 
+
 lexer = lex.lex()
+
 
 def getLexer():
     return lexer
