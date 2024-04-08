ee92e8a0ea47d5208be6cae3986451ccef7fa1cb
Juan Carlos Llanos <97850510+Juanllanos19@users.noreply.github.com>
2024-04-07 22:47:02 -0600

    Modificamos clite.py para pasar test_basic_integers

--- clite.py
+++ clite.py
@@ -3,3 +3,13 @@ import ply.lex as lex
 tokens = [ 'INT' ]
 
 t_ignore  = ' \t'
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+lexer = lex.lex()
+
+def getLexer():
+    return lexer
